812cdefb30b1f57924b1c21f9b1ded367134574f
IDCreativ <contact@id-creativ.com>
2021-12-29 17:34:39 +0100

    Installation et mise en place de nos ressources

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -7,13 +7,25 @@ use App\Repository\CustomerRepository;
 use ApiPlatform\Core\Annotation\ApiFilter;
 use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CustomerRepository::class)
- * @ApiResource
+ * @ApiResource(
+ *  collectionOperations={"GET", "POST"},
+ *  itemOperations={"GET", "PUT", "DELETE"},
+ *  subresourceOperations={
+ *      "invoices_get_subresource"={"path"="/customers/{id}/invoices"}
+ *  },
+ *  normalizationContext={
+ *      "groups"={"customers_read"}
+ *  }
+ * )
  * @ApiFilter(SearchFilter::class, properties={"firstName":"partial", "lastName":"partial", "company":"partial"})
  * @ApiFilter(OrderFilter::class)
  */
@@ -23,36 +35,53 @@ class Customer
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"customers_read", "invoices_read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read", "invoices_read"})
+     * @Assert\NotBlank(message="Le prénom du client est obligatoire.")
+     * @Assert\Length(min=3, max=255, minMessage="Le prénom doit faire entre 3 et 255 caractères.", maxMessage="Le prénom doit faire entre 3 et 255 caractères.")
      */
     private $firstName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read", "invoices_read"})
+     * @Assert\NotBlank(message="Le nom du client est obligatoire.")
+     * @Assert\Length(min=3, max=255, minMessage="Le nom doit faire entre 3 et 255 caractères.", maxMessage="Le nom doit faire entre 3 et 255 caractères.")
      */
     private $lastName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read", "invoices_read"})
+     * @Assert\NotBlank(message="L'adresse e-mail du client est obligatoire.")
+     * @Assert\Email(
+     *     message = "The email '{{ value }}' is not a valid email."
+     * )
      */
     private $email;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups({"customers_read", "invoices_read"})
      */
     private $company;
 
     /**
      * @ORM\OneToMany(targetEntity=Invoice::class, mappedBy="customer")
+     * @Groups({"customers_read"})
+     * @ApiSubresource
      */
     private $invoices;
 
     /**
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="customers")
+     * @Groups({"customers_read"})
+     * @Assert\NotBlank(message="L'utilisateur est obligatoire.")
      */
     private $user;
 
@@ -154,5 +183,27 @@ class Customer
         $this->user = $user;
 
         return $this;
+    }
+
+    /**
+     * Permet de récupérer le total des Invoices
+     * @Groups({"customers_read"})
+     */
+    public function getTotalAmount(): ?float
+    {
+        return array_reduce($this->invoices->toArray(), function($total, $invoice) {
+            return $total + $invoice->getAmount();
+        }, 0);
+    }
+
+    /**
+     * Permet de récupérer le total des Invoices non payées
+     * @Groups({"customers_read"})
+     */
+    public function getUnpaidAmount(): ?float
+    {
+        return array_reduce($this->invoices->toArray(), function($total, $invoice) {
+            return $total + ($invoice->getStatus() === "PAID" || $invoice->getStatus() === "CANCELLED" ? 0 : $invoice->getAmount());
+        }, 0);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,12 +7,16 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ApiResource
+ * @UniqueEntity("email", message="Un utilisateur utilise déjà cet e-mail.")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -20,11 +24,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"customers_read", "invoices_read", "invoices_subresources"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups({"customers_read", "invoices_read", "invoices_subresources"})
+     * @Assert\NotBlank(message="L'adresse e-mail de l'utilisateur est obligatoire.")
+     * @Assert\Email(
+     *     message = "L'email '{{ value }}' n'est pas valide."
+     * )
      */
     private $email;
 
@@ -36,16 +46,23 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * @Assert\NotBlank(message="Le mot de passe est obligatoire.")
      */
     private $password;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read", "invoices_read", "invoices_subresources"})
+     * @Assert\NotBlank(message="Le prénom de l'utilisateur est obligatoire.")
+     * @Assert\Length(min=3, max=255, minMessage="Le prénom doit faire entre 3 et 255 caractères.", maxMessage="Le prénom doit faire entre 3 et 255 caractères.")
      */
     private $firstName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read", "invoices_read", "invoices_subresources"})
+     * @Assert\NotBlank(message="Le nom de l'utilisateur est obligatoire.")
+     * @Assert\Length(min=3, max=255, minMessage="Le nom doit faire entre 3 et 255 caractères.", maxMessage="Le nom doit faire entre 3 et 255 caractères.")
      */
     private $lastName;
 
