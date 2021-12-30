ef1e1972c67fc5e201ba75160bb9864b054bf6af
IDCreativ <contact@id-creativ.com>
2021-12-30 13:13:11 +0100

    Ajout authentification via JWT

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -15,7 +15,9 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ApiResource
+ * @ApiResource(
+ *  normalizationContext={"groups"={"users_read"}}
+ * )
  * @UniqueEntity("email", message="Un utilisateur utilise déjà cet e-mail.")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
@@ -24,13 +26,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
-     * @Groups({"customers_read", "invoices_read", "invoices_subresources"})
+     * @Groups({"customers_read", "invoices_read", "invoices_subresources", "users_read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
-     * @Groups({"customers_read", "invoices_read", "invoices_subresources"})
+     * @Groups({"customers_read", "invoices_read", "invoices_subresources", "users_read"})
      * @Assert\NotBlank(message="L'adresse e-mail de l'utilisateur est obligatoire.")
      * @Assert\Email(
      *     message = "L'email '{{ value }}' n'est pas valide."
@@ -52,7 +54,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"customers_read", "invoices_read", "invoices_subresources"})
+     * @Groups({"customers_read", "invoices_read", "invoices_subresources", "users_read"})
      * @Assert\NotBlank(message="Le prénom de l'utilisateur est obligatoire.")
      * @Assert\Length(min=3, max=255, minMessage="Le prénom doit faire entre 3 et 255 caractères.", maxMessage="Le prénom doit faire entre 3 et 255 caractères.")
      */
@@ -60,7 +62,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"customers_read", "invoices_read", "invoices_subresources"})
+     * @Groups({"customers_read", "invoices_read", "invoices_subresources", "users_read"})
      * @Assert\NotBlank(message="Le nom de l'utilisateur est obligatoire.")
      * @Assert\Length(min=3, max=255, minMessage="Le nom doit faire entre 3 et 255 caractères.", maxMessage="Le nom doit faire entre 3 et 255 caractères.")
      */
